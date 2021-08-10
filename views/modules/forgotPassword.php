37c9bd471505fb37e38873a871a2062672753e3e
jm199-6 <joseantoniomendoza46@gmail.com>
2021-08-10 12:14:12 -0600

    Forgot password form

--- views/modules/forgotPassword.php
+++ views/modules/forgotPassword.php
@@ -1,19 +1,22 @@
 <div class="modal fade" id="modalPass" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <div class="modal-dialog">
-        <div class="modal-content">
-        	<form method="post" enctype='multipart/form-data'>
-                <div class="modal-header">
+        <form method="post" enctype='multipart/form-data'>
+        	<div class="modal-content">
+                <div class="modal-header bg-primary">
+                    <span class="modal-title text-white" id="myModalLabel">Recuperar Contraseña</span>
                     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                    <h4 class="modal-title" id="myModalLabel">Recuperar Contraseña</h4>
                 </div>
                 <div class="modal-body">
                 	<!-- CAMPOS DEL FORMULARIO-->
+                	
                     <div id="ndui" class="form-group input-group">
-						<span class="input-group-addon">
+                    	<div class="input-group-preppend">
+						<span class="input-group-text">
 							<font style="vertical-align:inherit;">
 								<i class="fa fa-user"></i>
 							</font>
 						</span>
+						</div>
 						<?php
 							/*$dui="";
 							$rO="";
@@ -22,14 +25,16 @@
 								$rO="readonly";
 							}*/
 						?>
-						<input required class='form-control' id="dui1" maxlength="10" type="text" name="dui_u1" value="<?php //echo desencript($dui); ?>" <?php //echo $rO; ?> placeholder="Numero de DUI" pattern="[0-9\-]{10,10}" title="Tamaño mínimo: 10. Tamaño máximo: 10" />
+						<input required class='form-control' onKeyUp="setGuion(1,'dui1')" id="dui1" maxlength="10" type="text" name="dui_u1" value="<?php //echo desencript($dui); ?>" <?php //echo $rO; ?> placeholder="Numero de DUI" pattern="[0-9\-]{10,10}" title="Tamaño mínimo: 10. Tamaño máximo: 10" />
 					</div>
 					<div id="nContra" class="form-group input-group">
-						<span class="input-group-addon">
+						<div class="input-group-preppend">
+						<span class="input-group-text">
 							<font style="vertical-align:inherit;">
 								<i class="fa fa-lock"></i>
 							</font>
 						</span>
+						</div>
 						<input required class='form-control' id="p1" type="password" name="p1" placeholder="Contraseña" pattern="[a-zA-Z0-9\-]{6,250}" title="La Contraseña debe contener a-z A-Z y numeros. Tamaño mínimo: 6. Tamaño máximo: 250" />
 						<span class="input-group-btn">
 							<button id="ver1" type="button" class="btn btn-default"><i class="fa fa-eye" id="icVer1"></i>&nbsp;</button>
@@ -37,11 +42,13 @@
 					</div>
 
 					<div id="nContraR" class="form-group input-group ">
-						<span class="input-group-addon">
+						<div class="input-group-preppend">
+						<span class="input-group-text">
 							<font style="vertical-align:inherit;">
 								<i class="fa fa-unlock-alt"></i>
 							</font>
 						</span>
+						</div>
 						<input required class='form-control' id="rP1" type="password" name="rp1" placeholder="Confirme Contraseña" pattern="[a-zA-Z0-9\-]{6,250}" title="La Contraseña debe contener a-z A-Z y numeros. Tamaño mínimo: 6. Tamaño máximo: 250" />
 						<span class="input-group-btn">
 								<button id="rVer1" type="button" class="btn btn-default"><i class="fa fa-eye" id="icRVer1"></i>&nbsp;</button>
@@ -62,8 +69,8 @@
                     <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
                     <button type="submit" name="savePass" value="ok" class="btn btn-primary">Guardar Cambios</button>
                 </div>
-            </form>
-        </div>
+        	</div>
+        </form>
         <!-- /.modal-content -->
     </div>
 
